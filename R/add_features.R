19dfc29f20a6ab82842d07264fb50e81520d854d
Tom Elliott <tom.elliott@auckland.ac.nz>
2023-06-23 15:06:52 +1200

    Add line

--- R/add_features.R
+++ R/add_features.R
@@ -27,6 +27,36 @@ add_rect <- function(file, x0, y0, x1, y1,
     magick::image_write(img_e, name)
 }
 
+add_line <- function(file, x0, y0, x1 = x0, y1 = y0,
+                     line_col = "red",
+                     line_width = 2L
+                     ) {
+    if (missing(file))
+        file <- getOption("screenshot.last.file")
+
+    if (is.null(file))
+        stop("No file specified")
+
+    dir <- dirname(file)
+    name <- sprintf(
+        "%s.png",
+        file.path(dir, file)
+    )
+    if (!file.exists(name)) stop("File does not exist")
+
+    cli::cli_progress_step("Add line")
+
+    img <- magick::image_read(name)
+
+    img_e <- magick::image_draw(img)
+    lines(x0, y0, x1, y1,
+        lwd = line_width,
+        col = line_col
+    )
+    dev.off()
+    magick::image_write(img_e, name)
+}
+
 add_arrow <- function(file, x0, y0, x1 = x0, y1 = y0,
                       arrow_col = "red",
                       arrow_width = 2L,
